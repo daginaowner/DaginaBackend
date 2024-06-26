54f24487d127d6af08a82171e4bf37236011e393
Dash10107 <dakshcjain@gmail.com>
2024-06-26 16:42:54 +0530

    user py

--- api/user.py
+++ api/user.py
@@ -4,11 +4,16 @@ from services.userService import signup_service, login_service
 user_route = Blueprint('user_route', __name__)
 
 @user_route.route("api/user/buyer/signup", methods=['POST'])
-def signup():
+def buyerSignup():
     data = request.get_json()
     return signup_service(data)
 
-@user_route.route("api/user/seller/login", methods=['POST'])
+@user_route.route("api/user/seller/signup", methods=['POST'])
+def sellerSignup():
+    data = request.get_json()
+    return login_service(data)
+
+@user_route.route("api/user/login", methods=['POST'])
 def login():
     data = request.get_json()
     return login_service(data)

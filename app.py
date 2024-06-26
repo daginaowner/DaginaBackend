ddd1cea850a8277eac9326262fa1df100470c52f
Dash10107 <dakshcjain@gmail.com>
2024-06-26 16:48:27 +0530

    connected app route for user

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import dotenv_values
-
+from api.user import user_route
 app = Flask(__name__)
 
 config = dotenv_values(".env")
@@ -14,6 +14,7 @@ client = MongoClient(mongo_uri)
 db = client.get_default_database(db_name)
 
 CORS(app)
+app.register_blueprint(user_route)
 
 @app.route('/', methods=['GET'])
 def hello():

cb41bb8ade52a3df4fe96612aff9b81549568763
Dash10107 <dakshcjain@gmail.com>
2024-06-27 17:12:23 +0530

    created routes for products

--- api/product_api.py
+++ api/product_api.py
@@ -14,39 +14,39 @@ produc_route = Blueprint('product_route', __name__)
 
 
 #getting general products
-@user_route.route("api/products", methods=['GET'])
+@produc_route.route("api/products", methods=['GET'])
 def get_products():
     # data = request.get_json()
     return get_products_service()
 
 #getting products based on category
-@user_route.route("api/products/<string:category>", methods=['GET'])
+@produc_route.route("api/products/<string:category>", methods=['GET'])
 def get_categories_products(category):
     # data = request.get_json()
     return get_categories_products_service(category,DB)
 
 #getting products based on price range
-@user_route.route("api/products/<string:pricerange>", methods=['GET'])
+@produc_route.route("api/products/<string:pricerange>", methods=['GET'])
 def get_price_range_products(pricerange):
     return get_price_range_products_service(pricerange,DB)
 
 #getting products based on seller Id
-@user_route.route("api/products/<string:sellerId>", methods=['GET'])
+@produc_route.route("api/products/<string:sellerId>", methods=['GET'])
 def get_seller_products(sellerId):
     return get_seller_products_service(sellerId,DB)
 
 #getting producst based on clicks
-@user_route.route("api/products/clicks", methods=['GET'])
+@produc_route.route("api/products/clicks", methods=['GET'])
 def get_clicks_products():
     # data = request.get_json()
     return get_clicks_products_service(DB)
 
 #getting single product details
-@user_route.route("api/products/<string:productId>", methods=['GET'])
+@produc_route.route("api/products/<string:productId>", methods=['GET'])
 def get_price_range_products(productId):
     return get_product_details_service(productId,DB)
 
 #finding products details based on search field
-@user_route.route("api/products/<string:searchInput>", methods=['GET'])
+@produc_route.route("api/products/<string:searchInput>", methods=['GET'])
 def get_price_range_products(searchInput):
     return get_searched_products_service(searchInput,DB)

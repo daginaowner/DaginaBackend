b30e378d8d8aceb0ff6af31d906794391d1466d2
Vedant Rajendra Shetye <vedantblackops@gmail.com>
2024-09-17 00:13:26 +0530

    Structured basics and envs

--- api/mongo_connect.py
+++ api/mongo_connect.py
@@ -5,6 +5,14 @@ config = dotenv_values(".env")
 mongo_uri = config.get("MONGO_URI")
 db_name = config.get("DB_NAME")
 client = MongoClient(mongo_uri)
-DB = client.get_default_database(db_name)
+DB = None
 
-print("Connected to the MongoDB database!")
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(f"An error occured while connecting \n{e}")
+
+DB = client.get_default_database(db_name)
+print("Connected to the Dagina database!")

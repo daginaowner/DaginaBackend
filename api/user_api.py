b6eeb9cb2067bd9f82fbcb6141cdf89ac4c8db44
Vedant Rajendra Shetye <vedantblackops@gmail.com>
2024-09-17 00:32:15 +0530

    Whole structure change incoming

--- api/user_api.py
+++ api/user_api.py
@@ -4,7 +4,7 @@ from .mongo_connect import DB
 
 user_route = Blueprint('user_route', __name__)
 
-@user_route.route("/api/user/buyer/signup", methods=['POST'])
+@user_route.route("/buyer/signup", methods=['POST'])
 def buyer_signup():
     data = request.get_json()
     return buyer_signup_service(data)
@@ -14,12 +14,7 @@ def seller_signup():
     data = request.get_json()
     return seller_signup_service(data)
 
-"""
-In this endpoint 
-0 indicates login for Buyer
-any other integer indicates login for Seller
-"""
-@user_route.route("/api/user/login/<int:type>", methods=['POST'])
+@user_route.route("/buyer/login", methods=['POST'])
 def buyer_login(type):
     data = request.get_json()
     return user_login_service(data,type)
